a0f9db74cd8400f96863ba68c0fe6ece691b6fd7
Walid CHERGUI <walid.chergui@gmail.com>
2016-09-06 09:42:18 +0200

    add implementation of list of float

---
+++ src/test/java/com/fabulouslab/collection/BlackoutFloatListTest.java
@@ -0,0 +1,32 @@
+package com.fabulouslab.collection;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+public class BlackoutFloatListTest {
+
+    @Test
+    public void should_get_3_as_size() throws Exception {
+
+        BlackoutFloatList blackoutList = new BlackoutFloatList(0.0f,1.0f,2.0f);
+
+        assertThat(blackoutList.size()).isEqualTo(3);
+        assertThat(blackoutList.isEmpty()).isFalse();
+        assertThat(blackoutList.get(0)).isEqualTo(0.0f);
+        assertThat(blackoutList.get(1)).isEqualTo(1.0f);
+        assertThat(blackoutList.get(2)).isEqualTo(2.0f);
+    }
+
+    @Test
+    public void should_set_new_value() throws Exception {
+
+        BlackoutFloatList blackoutList = new BlackoutFloatList(0.0f,1.0f,2.0f);
+        float oldValue = blackoutList.set(2,3.0f);
+
+        assertThat(oldValue).isEqualTo(2.0f);
+        assertThat(blackoutList.get(2)).isEqualTo(3.0f);
+    }
+
+}
